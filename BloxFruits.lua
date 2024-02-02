027271ff4dfc7dfcc705f241ead2514e5e9f31e1
SewBMango <157802897+SewBMango@users.noreply.github.com>
2024-02-02 14:21:43 +0700

    Add files via upload

---
+++ BloxFruits.lua
@@ -0,0 +1,158 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Rubik Hub - Blox Fruits",
+    SubTitle = "by SewB",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(595, 375),
+    Acrylic = true,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+    TabShop = Window:AddTab({ Title = "Tab Shop", Icon = "" }),
+    TabFarm = Window:AddTab({ Title = "Tab Farm", Icon = "" }),
+    TabMisc = Window:AddTab({ Title = "Tab Misc", Icon = "" }),
+}
+
+local Options = Fluent.Options
+
+Tabs.TabShop:AddButton({
+    Title = "Redeem Code"
+})
+
+Tabs.TabShop:AddParagraph({
+    Title = "Fighting Shop",
+    Content = "Melee Shop"
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Black Leg",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyBlackLeg")
+    end
+})
+
+
+Tabs.TabShop:AddButton({
+    Title = "Fishman Karate",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyFishmanKarate")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Electro",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyElectro")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Dragon Breath",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDragonBreath")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "SuperHuman",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySuperHuman")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Death Step",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDeathStep")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Sharkman Karate",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySharkmanKarate")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Electric Claw",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyElectricClaw")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Dragon Talon",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDragonTalon")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "God Human",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyGodHuman")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Sanguine Art",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySanguineAre")
+    end
+})
+
+Tabs.TabShop:AddParagraph({
+    Title = "Abilities Shop",
+    Content = "Abilities Shop"
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Sky Jump [ $10.000 Beli ]",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySkyJump")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Buso Haki [ $25.000 Beli ]",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyBusoHaki")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Observation Haki [ $750.000 Beli ]",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyObservationHaki")
+    end
+})
+
+Tabs.TabShop:AddButton({
+    Title = "Soru [ $100.000 Beli]",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuySoru")
+    end
+})
+
+local TradeBone = Tabs.TabMisc:AddToggle("TradeBone", {Title = "Trade Bone", Default = false})
+   TradeBone:OnChanged(function(value)
+        _G.RandomBoneFunction = value
+    end)
+
+   Options.TradeBone:SetValue(false)
+
+   spawn(function()
+    pcall(function()
+        while wait(0.25) do
+            if _G.RandomBoneFunction then    
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Bones","Buy",1,1)
+            end
+        end
+    end)
+end)
